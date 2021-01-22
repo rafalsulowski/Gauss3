6858a6212eeab07d27bfacd9f6e58b779c7fd1d7
Rafał Sulowski <48453164+rafalsulowski@users.noreply.github.com>
2021-01-22 21:43:16 +0100

    final version

--- gauss.c
+++ gauss.c
@@ -7,7 +7,6 @@ int eliminate(int n, double **AB)
 {
   int i, j, k;
   double m, tmp;
-
   // Metoda eliminacju gaussa
   // eliminacja współczynników
 
@@ -39,7 +38,7 @@ int eliminate(int n, double **AB)
     // Elimination of variables
     for (i = k + 1; i < n; i++)
     {
-      double m = AB[i][k] / AB[k][k];
+      m = AB[i][k] / AB[k][k];
       for (j = k; j < n + 1; j++)
         AB[i][j] -= m * AB[k][j];
     }
